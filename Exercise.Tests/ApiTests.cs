614a3adff062b3ce0179785eee47d511462cc61d
mrjohnsonit <mr.johnson.it@gmail.com>
2017-09-14 21:25:50 -0400

    Adding files

---
+++ Exercise.Tests/ApiTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise.Api;
+using Exercise.Domain.Api;
+using Moq;
+using NUnit.Framework;
+
+namespace Exercise.Tests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        /// <summary>
+        /// Testing the concrete instance of IMessageProvider.
+        /// </summary>
+        [Test]
+        public void MessageProviderTest()
+        {
+            var messageProvider = new StringMessageProvider() as IMesssageProvider;
+
+            Assert.AreEqual(
+                StringMessageProvider.HelloWorld, 
+                messageProvider.GetHelloWorldMessage());
+        }
+
+        /// <summary>
+        /// Testing the concrete instance of IMessageService.
+        /// This uses a mock instance of IMessageProvider.
+        /// </summary>
+        [Test]
+        public void MessageServiceTest()
+        {
+            //create mocked provider
+            var providerMessage = "testing 123";
+            var providerMock = new Mock<IMesssageProvider>();
+            var messageService = new MessageService(providerMock.Object) as IMessageService;
+
+            //setup the mock return on the provider
+            providerMock.Setup(x => x.GetHelloWorldMessage()).Returns(providerMessage);
+            
+            //call the service which should return the value in providerMessage
+            Assert.AreEqual(providerMessage, messageService.GetHelloWorldMessage());
+
+            //verify the provider was called (should really be expected if the previous statement doesn't fail
+            providerMock.Verify(x => x.GetHelloWorldMessage(), Times.Once);
+
+            ////this would fail if uncommented
+            //providerMock.Verify(x => x.GenerateMessage(), Times.Exactly(2)); 
+        }
+
+
+    }
+}
---
+++ Exercise.Tests/AppTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise.App.ToConsole;
+using Exercise.Domain.App;
+using NUnit.Framework;
+
+namespace Exercise.Tests
+{
+    [TestFixture]
+    public class AppTests
+    {
+        [Test]
+        public void ToConsoleTest()
+        {
+            using (var sw = new StringWriter())
+            {
+                //set the console to write to the string writer
+                Console.SetOut(sw);
+
+                //setup input and outputs
+                var inputValue = "from the console";
+                var expectedValue = $"{inputValue}\r\n";
+
+                //create instance of console app
+                var consoleApp = new ToConsole() as IExerciseApp;
+
+                //call console app method
+                consoleApp.WriteMessage(inputValue);
+
+                //verify
+                Assert.AreEqual(expectedValue, sw.ToString());
+            }
+        }
+
+    }
+}
